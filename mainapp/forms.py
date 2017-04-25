16f8ded561d822b8ce16bf30fdbc80c7c23fa005
Mateusz Kosiba <kosiba.mat@gmail.com>
2017-04-25 18:42:29 +0200

    From for the search view

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,12 +1,17 @@
 import flask_wtf
 import wtforms
+import wtforms.fields.html5
 
 
-class SearchForm(flask_wtf.Form):
-    imie = wtforms.StringField('imie')
-    nazwisko = wtforms.StringField('nazwisko')
-    keywords = wtforms.StringField('keywords')
-    publikacja = wtforms.StringField('publikacja')
-    afilacja = wtforms.StringField('afilacja')
-    od = wtforms.DateField('start', format="%Y")
-    do = wtforms.DateField('koniec', format="%Y")
+class SearchForm(flask_wtf.FlaskForm):
+    imie = wtforms.StringField('Name')
+    nazwisko = wtforms.StringField('Surname')
+    keywords = wtforms.StringField('Interests')
+    publikacja = wtforms.StringField('Publication')
+    afilacja = wtforms.StringField('Institution/University')
+    #od = wtforms.fields.html5.DateField('Publication dates (from)', format="%Y")
+    od = wtforms.fields.SelectField('Publication year (From)', coerce=int)
+    do = wtforms.fields.SelectField('Publication year (To)', coerce=int)
+    datesearch = wtforms.fields.BooleanField(label="Deactivate publication date filtering", default=True)
+    # TODO
+    # Multichoice for resources
